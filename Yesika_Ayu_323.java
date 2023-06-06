f455a2538284ac430fb55a0bcc429e81465481d8
Yesi323 <133621101+Yesi323@users.noreply.github.com>
2023-06-06 14:28:20 +0700

    Add files via upload

---
+++ Yesika_Ayu_323.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yesika_ayu_323;
+
+
+public class Yesika_Ayu_323 {
+
+    public static void main(String[] args) {
+        System.out.println("Yesika Ayu Yuliana (2113010323)");
+    }
+}
