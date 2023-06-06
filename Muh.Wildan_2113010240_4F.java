9c488d8cf1848eedf4b979f53fc06f146a8eab2b
Wildanmubarok <135305151+Wildanmubarok@users.noreply.github.com>
2023-06-06 12:44:26 +0700

    Add files via upload

---
+++ Muh.Wildan_2113010240_4F.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m.wildan240.pkg4f;
+
+/**
+ *
+ * @author Marcelinus Raphael B
+ */
+public class MWildan2404F {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("M.Wildan (2113010240)");
+    }
+    
+}
