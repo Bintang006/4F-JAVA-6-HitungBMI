dbeb5e67c3a5be58655df134afad9ef217d4395e
Bintang006 <122715207+Bintang006@users.noreply.github.com>
2023-06-06 12:38:35 +0700

    Add files via upload

---
+++ BintangRadiityoSeto_2113010258_4F.java
@@ -0,0 +1,11 @@
+
+package tugas_individu_bintang.radiityo.seto_2113010258_4f;
+
+
+public class Tugas_Individu_BintangRadiityoSeto_2113010258_4F {
+
+    public static void main(String[] args) {
+        System.out.println("Bintang Radityo Seto (2113010258)");
+    }
+    
+}
