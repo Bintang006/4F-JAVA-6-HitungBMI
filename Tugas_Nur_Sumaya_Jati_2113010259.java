4ce83884d5328a389cb58bf82d1de06a065ebae9
maya259 <133621506+maya259@users.noreply.github.com>
2023-06-06 12:45:47 +0700

    Add files via upload

---
+++ Tugas_Nur_Sumaya_Jati_2113010259.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas_nur_sumaya_jati_2113010259;
+
+/**
+ *
+ * @author Naya
+ */
+public class Tugas_Nur_Sumaya_Jati_2113010259 {
+
+    public static void main(String[] args) {
+        System.out.println("Nur Sumaya Jati (2113010259)");
+    }
+}
